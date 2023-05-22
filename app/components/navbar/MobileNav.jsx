7f473a03663373a281e1a85c6f207a3ac37fc056
twinx97 <joonatan.karhu@outlook.com>
2023-05-22 14:17:33 +0200

    Added navheight state

--- app/components/navbar/MobileNav.jsx
+++ app/components/navbar/MobileNav.jsx
@@ -1,7 +1,17 @@
-
-const MobileNav = () => {
+const MobileNav = ({ navHeight }) => {
+  console.log(navHeight)
   return (
-    <div>MobileNav</div>
+    <div className={`absolute top-[${navHeight}px] w-full bg-white rounded-md shadow-lg py-2 z-10`}>
+      <a href="#" className="block px-4 py-2 text-gray-800 hover:bg-gray-100">
+        Menu Item 11
+      </a>
+      <a href="#" className="block px-4 py-2 text-gray-800 hover:bg-gray-100">
+        Menu Item 2
+      </a>
+      <a href="#" className="block px-4 py-2 text-gray-800 hover:bg-gray-100">
+        Menu Item 3
+      </a>
+    </div>
   )
 }
 
--- app/components/navbar/Navbar.jsx
+++ app/components/navbar/Navbar.jsx
@@ -1,14 +1,25 @@
 'use client'
 import Image from 'next/image'
 import Hamburger from './Hamburger'
-import MobileNav from "./MobileNav"
-import { useState } from 'react'
+import MobileNav from './MobileNav'
+import { useEffect, useRef, useState } from 'react'
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false)
+  const [navHeight, setNavHeight] = useState(0)
+
+  const componentRef = useRef(null)
+  let componentHeight = 0
+
+  useEffect(() => {
+    if (componentRef.current) {
+      componentHeight = componentRef.current.offsetHeight
+      setNavHeight(componentHeight)
+    }
+  }, [])
 
   return (
-    <div className="flex justify-between w-full px-5">
+    <div ref={componentRef} className="flex justify-between w-full px-5 bg-red-600">
       <div>
         <Image
           src="/profile_picture.png"
@@ -18,29 +29,7 @@ const Navbar = () => {
         />
       </div>
       <Hamburger isOpen={isOpen} setIsOpen={setIsOpen} />
-      {isOpen && (
-        <div className="absolute top-0 right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-2 z-10">
-          {/* Add your menu items here */}
-          <a
-            href="#"
-            className="block px-4 py-2 text-gray-800 hover:bg-gray-100"
-          >
-            Menu Item 1
-          </a>
-          <a
-            href="#"
-            className="block px-4 py-2 text-gray-800 hover:bg-gray-100"
-          >
-            Menu Item 2
-          </a>
-          <a
-            href="#"
-            className="block px-4 py-2 text-gray-800 hover:bg-gray-100"
-          >
-            Menu Item 3
-          </a>
-        </div>
-      )}
+      {isOpen && <MobileNav navHeight={navHeight} />}
     </div>
   )
 }
