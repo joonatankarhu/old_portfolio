5729f84051c122f5e18ca4980d8bc0b475b1d3b0
twinx97 <joonatan.karhu@outlook.com>
2023-05-25 17:38:03 +0200

    added polyfil for smoothscroll

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,12 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Navbar from './components/navbar/Navbar'
+import smoothscroll from 'smoothscroll-polyfill'
 
 const inter = Inter({ subsets: ['latin'] })
+
+// Call the polyfill function
+
 
 export const metadata = {
   title: 'Joonatan Karhu',
@@ -17,8 +21,9 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  smoothscroll.polyfill()
   return (
-    <html lang="en" style={{scrollBehavior:'smooth'}}>
+    <html lang="en" style={{ scrollBehavior: 'smooth' }}>
       <body className={inter.className}>
         <Navbar />
         {children}
