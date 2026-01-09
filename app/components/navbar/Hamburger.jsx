a3617504a6e9108606bd4ef499390b2010ca30e7
Joonatan Karhu <joonatan.karhu@outlook.com>
2026-01-09 12:44:14 +0200

    Add hamburger transition styles

--- app/components/navbar/Hamburger.jsx
+++ app/components/navbar/Hamburger.jsx
@@ -1,45 +1,53 @@
-'use client'
-
-import { useState } from "react";
+"use client";
 
 const Hamburger = ({ isOpen, setIsOpen }) => {
-  const [isHovered, setIsHovered] = useState(false);
-
-  const handleHover = () => {
-    setIsHovered(!isHovered);
-  };
-  const handleToggle = () => {
-    setIsOpen(!isOpen)
-  }
-
   return (
-    <div className="relative ml-auto lg:hidden z-[100]">
-      <button
-        type="button"
-        onClick={handleToggle}
-        className={`text-black/70 hover:text-blue-700 block focus:outline-none transition-transform duration-1000 `}
-        onMouseEnter={handleHover}
-        onMouseLeave={handleHover}
-      >
-        <svg
-          className="w-8 h-10"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2.3"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        >
-          {isOpen ? (
-            <path d="M18 6L6 18M6 6l12 12" />
-          ) : (
-            <path d="M3 12h18M3 6h18M3 18h18" />
-          )}
-        </svg>
-      </button>
-    </div>
-  )
-}
+    <button
+      type="button"
+      aria-label="Toggle menu"
+      onClick={() => setIsOpen(!isOpen)}
+      className="
+        ml-auto relative z-[100]
+        flex flex-col justify-center items-center
+        w-10 h-10
+        lg:hidden
+        group
+      "
+    >
+      {/* Top line */}
+      <span
+        className={`
+          block w-7 h-[2.5px] bg-black
+          transition-all duration-300 ease-in-out
+          ${
+            isOpen ? "rotate-45 translate-y-[7px]" : "group-hover:translate-y-1"
+          }
+        `}
+      />
 
-export default Hamburger
+      {/* Middle line */}
+      <span
+        className={`
+          block w-7 h-[2.5px] bg-black my-[5px]
+          transition-all duration-300 ease-in-out
+          ${isOpen ? "opacity-0" : "group-hover:opacity-70"}
+        `}
+      />
+
+      {/* Bottom line */}
+      <span
+        className={`
+          block w-7 h-[2.5px] bg-black
+          transition-all duration-300 ease-in-out
+          ${
+            isOpen
+              ? "-rotate-45 -translate-y-[7px]"
+              : "group-hover:-translate-y-1"
+          }
+        `}
+      />
+    </button>
+  );
+};
+
+export default Hamburger;
